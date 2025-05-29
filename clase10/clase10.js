6515e671221e083d09ba700d10a2a90d02909cc1
PaoGazzia <paola_gazzia@hotmail.com>
2025-05-28 21:52:22 -0300

    otra clase

---
+++ clase10/clase10.js
@@ -0,0 +1,19 @@
+//Simular un sistema de ingreso a una nave espacial donde los tripulantes deben pasar filtros de validación usando variables, coerción, y operadores lógicos.
+//🧩 Desarrollo:
+//Pedí a tus alumnas que creen un script que haga lo siguiente:
+// IF TERNARIO
+
+const prompt = require('prompt-sync')();
+
+let nombre = prompt("Ingrese su nombre galactico: ");
+let edad = parseInt(prompt("Cual es tu edad terrestre: ")); 
+let especie = prompt("Cual es tu especie: (Humano, Alien, Cyborg) ");
+
+
+let tienePermiso = (edad>=18 && especie!=="") || nombre === "Capitana";
+
+const mensaje = tienePermiso
+    ? "Bienvenido a bordo " + (nombre || "tripulante desconocido") + "!"
+    : "Acceso denegado, no podes ingresar a la nave";
+
+console.log(mensaje);
